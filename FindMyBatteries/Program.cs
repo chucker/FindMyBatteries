9788eb98e4c9adbd22dc92f594473082d3cd81bd
Sören Kuklau <chucker23n+github@gmail.com>
2021-06-02 00:22:57 +0200

    Initial

---
+++ FindMyBatteries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMyBatteries
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
