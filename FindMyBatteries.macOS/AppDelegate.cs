f02ebda9af920702f747e6f443e2dafbbaec9f6f
Sören Kuklau <chucker23n+github@gmail.com>
2021-06-03 09:35:43 +0200

    We now create a primitive status item that contains device names and their battery levels. Cool

--- FindMyBatteries.macOS/AppDelegate.cs
+++ FindMyBatteries.macOS/AppDelegate.cs
@@ -1,4 +1,8 @@
-﻿using AppKit;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AppKit;
 using Foundation;
 
 namespace FindMyBatteries.macOS
@@ -6,18 +10,95 @@ namespace FindMyBatteries.macOS
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
+        private NSStatusItem? _StatusItem;
+
         public AppDelegate()
         {
         }
 
         public override void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to initialize your application
+            new System.Threading.Timer(async _ =>
+            {
+                var findMeResponse = await GetFindMeDataAsync();
+
+                InvokeOnMainThread(() => CreateStatusBarItem(findMeResponse));
+            }, null, 0, Timeout.Infinite);
+        }
+
+        private async Task<FindMe.DTOs.FindMeResponse> GetFindMeDataAsync()
+        {
+            var user = (await File.ReadAllTextAsync("user.txt")).Trim();
+            var pw = (await File.ReadAllTextAsync("pw.txt")).Trim();
+
+            // based on https://github.com/MauriceConrad/iCloud-API
+
+            var iCloudAuth = new ICloud.ICloudAuth();
+            await iCloudAuth.InitSessionTokenAsync(user, pw);
+            await iCloudAuth.AccountLoginAsync();
+
+            return await new FindMe.FindMe().InitClientAsync(iCloudAuth);
         }
 
         public override void WillTerminate(NSNotification notification)
         {
             // Insert code here to tear down your application
+        }
+
+        private void CreateStatusBarItem(FindMe.DTOs.FindMeResponse findMeResponse)
+        {
+            NSStatusBar statusBar = NSStatusBar.SystemStatusBar;
+
+            this._StatusItem = statusBar.CreateStatusItem(NSStatusItemLength.Variable);
+            _StatusItem.Title = "Test";
+            //_StatusItem.Image = EFontAwesomeIcon.Solid_Music.GetNSImage();
+            _StatusItem.Menu = new NSMenu();
+
+            //_StatusItem.Button.AddGestureRecognizer(new NSPressGestureRecognizer(CenterSearchWindow)
+            //{
+            //    MinimumPressDuration = 1
+            //});
+
+            string title;
+            NSMenuItem menuItem;
+
+            foreach (var item in findMeResponse.Content!)
+            {
+                if (item.BatteryStatus == "Unknown")
+                    continue;
+
+                menuItem = new NSMenuItem(item.Name);
+                _StatusItem.Menu.AddItem(menuItem);
+
+                string suffix = item.BatteryStatus == "Charging" ? " (charging)" : "";
+
+                menuItem = new NSMenuItem($"    {item.BatteryLevel:P0}{suffix}");
+                _StatusItem.Menu.AddItem(menuItem);
+            }
+
+            //            if (iCloudAuth.TfaRequired)
+            //            {
+            //                Console.WriteLine("Enter two-factor code:");
+            //var securityCode=                Console.ReadLine();
+            //                await iCloudAuth.EnterSecurityCodeAsync(securityCode);
+            //            }
+
+
+            title = "Preferences…";
+            //menuItem = new NSMenuItem(title, ",", (sender, e) =>
+            //{
+            //    DependencyService.Get<Services.Windows.PreferencesWindowService>().OpenWindow();
+            //});
+            //_StatusItem.Menu.AddItem(menuItem);
+
+            _StatusItem.Menu.AddItem(NSMenuItem.SeparatorItem);
+
+            //title = $"Quit {AppName}";
+            menuItem = new NSMenuItem(title, "q", (sender, e) =>
+            {
+                NSApplication.SharedApplication.Terminate(sender as NSObject);
+            });
+            _StatusItem.Menu.AddItem(menuItem);
         }
     }
 }
