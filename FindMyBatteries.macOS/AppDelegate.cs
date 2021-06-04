9f648f83b4e95792a088786ae4ae44859ce680a1
Sören Kuklau <chucker23n+github@gmail.com>
2021-06-04 19:56:59 +0200

    keep refreshing battery states: remove various experiments (e.g. ObservableCollection)

--- FindMyBatteries.macOS/AppDelegate.cs
+++ FindMyBatteries.macOS/AppDelegate.cs
@@ -1,21 +1,16 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
 using AppKit;
 using FindMyBatteries.FindMe.DTOs;
 using Foundation;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace FindMyBatteries.macOS
 {
     [Register("AppDelegate")]
     public class AppDelegate : NSApplicationDelegate
     {
-        //private NSString _Separator = new NSString();
-
         private NSStatusItem? _StatusItem;
 
         public Device[]? Devices { get; private set; }
@@ -32,23 +27,6 @@ namespace FindMyBatteries.macOS
                 // we could use ObservableCollection here, but there seems to be little real benefit
                 Devices = (await GetFindMeDataAsync()).Content;
 
-                //var responseByName = findMeResponse.Content.ToDictionary(d => d.Name);
-                //var localByName = Devices.ToDictionary(d => d.Name);
-
-                //foreach (var item in localByName.Keys)
-                //{
-                //    if (!responseByName.ContainsKey(item))
-                //        Devices.Remove(localByName[item]);
-                //}
-
-                //foreach (var item in responseByName)
-                //{
-                //    if (localByName.ContainsKey(item.Key))
-                //        Devices.Remove(localByName[item.Key]);
-
-                //    Devices.Add(item.Value);
-                //}
-
                 InvokeOnMainThread(() => RefreshMenuItems());
             }, null, TimeSpan.FromSeconds(0), TimeSpan.FromMinutes(1));
 
@@ -59,7 +37,7 @@ namespace FindMyBatteries.macOS
         {
             if (Devices == null)
             {
-                // log.warn
+                // TODO log.warn
                 return;
             }
 
@@ -93,61 +71,6 @@ namespace FindMyBatteries.macOS
                 j++;
             }
         }
-
-        //private void Devices_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        //{
-        //    InvokeOnMainThread(() =>
-        //    {
-        //        Console.WriteLine($"Collection changed");
-
-        //        if (_StatusItem?.Menu == null)
-        //            return;
-
-        //        if (e.OldItems != null)
-        //        {
-        //            Console.WriteLine($"Removing {e.OldItems.Count} items");
-
-        //            foreach (var item in e.OldItems)
-        //            {
-        //                var firstItem = _StatusItem.Menu.Items?.FirstOrDefault(mi => mi.Title == ((Device)item).Name);
-
-        //                if (firstItem != null)
-        //                {
-        //                    var index = _StatusItem.Menu.IndexOfItem(firstItem);
-
-        //                    // remove two items starting from this point
-        //                    _StatusItem.Menu.RemoveItemAt(index);
-        //                    _StatusItem.Menu.RemoveItemAt(index);
-        //                }
-        //            }
-        //        }
-
-        //        if (e.NewItems != null)
-        //        {
-        //            Console.WriteLine($"Adding {e.NewItems.Count} items");
-
-        //            foreach (var device in e.NewItems.OfType<Device>().OrderBy(d => d.Name))
-        //            {
-        //                if (device.BatteryStatus == "Unknown")
-        //                    continue;
-
-        //                NSMenuItem firstSeparator = _StatusItem.Menu.Items.FirstOrDefault(i => i.IsSeparatorItem);
-        //                var separatorIndex = _StatusItem.Menu.IndexOfItem(firstSeparator);
-
-        //                if (separatorIndex < 1)
-        //                    separatorIndex = 1;
-
-        //                var menuItem = new NSMenuItem(device.Name);
-        //                _StatusItem.Menu.InsertItem(menuItem, separatorIndex - 1);
-
-        //                string suffix = device.BatteryStatus == "Charging" ? " (charging)" : "";
-
-        //                menuItem = new NSMenuItem($"    {device.BatteryLevel:P0}{suffix}");
-        //                _StatusItem.Menu.InsertItem(menuItem, separatorIndex);
-        //            }
-        //        }
-        //    });
-        //}
 
         private async Task<FindMeResponse> GetFindMeDataAsync()
         {
@@ -192,24 +115,6 @@ namespace FindMyBatteries.macOS
             string title;
             NSMenuItem menuItem;
 
-            //foreach (var item in findMeResponse.Content!)
-            //{
-            //    if (item.BatteryStatus == "Unknown")
-            //        continue;
-
-            //    menuItem = new NSMenuItem(item.Name);
-            //    _StatusItem.Menu.AddItem(menuItem);
-
-            //    string suffix = item.BatteryStatus == "Charging" ? " (charging)" : "";
-
-            //    menuItem = new NSMenuItem($"    {item.BatteryLevel:P0}{suffix}");
-            //    _StatusItem.Menu.AddItem(menuItem);
-            //}
-
-            //var separator =(NSMenuItem) NSMenuItem.SeparatorItem.Copy();
-            //separator.RepresentedObject = _Separator;
-
-            //_StatusItem.Menu.AddItem(separator);
             _StatusItem.Menu.AddItem(NSMenuItem.SeparatorItem);
 
             title = $"Quit Find My Batteries";
@@ -218,10 +123,6 @@ namespace FindMyBatteries.macOS
                 NSApplication.SharedApplication.Terminate(sender as NSObject);
             });
             _StatusItem.Menu.AddItem(menuItem);
-            //menuItem.repre
-            //var menuBar = NSApplication.SharedApplication.MainMenu;
-            //var item = menuBar.ItemWithTitle("File");
-            //var index = menuBar.IndexOfItem(item);
         }
     }
 }
